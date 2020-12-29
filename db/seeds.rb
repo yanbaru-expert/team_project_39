ec222bd10aa1ff4eb59ea7b49a50f8bc95bb8bcd
kawakami-masachika <supercell1023koeda@gmail.com>
2020-12-29 12:44:43 +0900

    テキストモデルの初期データをCSVからインポートする処理を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,20 @@ User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
 end
+
+require 'csv'
+
+# テキストモデルのデータをインポート
+Text.destroy_all
+
+# ファイルパス取得
+text_file_path = 'db/csv_data/text_data.csv'
+option = {headers: true}
+text_list = []
+
+CSV.foreach(text_file_path, option) do |row|
+  text_list << row.to_h
+end
+
+Text.create!(text_list)
+puts  'テキストモデルの初期データインポートに成功しました。'
