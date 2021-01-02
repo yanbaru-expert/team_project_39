4dc1fae78f971057141336110314f46bbe298a9e
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-01-03 00:18:57 +0900

    テキスト教材一覧ページのスタイルをapplication.scssファイルに統一

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,8 +1,7 @@
 class TextsController < ApplicationController
   def index
-    # genre_list = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
-    # @texts = Text.where(genre: genre_list)
+    genre_list = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
+    @texts = Text.where(genre: genre_list)
 
-    @texts = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
   end
 end
