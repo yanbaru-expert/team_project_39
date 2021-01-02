d89b850bd37a936b408d587110de87ddccf127e0
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-01-02 23:48:19 +0900

    ルーティングとコントローラーを設定

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,4 +1,8 @@
 class TextsController < ApplicationController
   def index
+    # genre_list = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
+    # @texts = Text.where(genre: genre_list)
+
+    @texts = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
   end
 end
