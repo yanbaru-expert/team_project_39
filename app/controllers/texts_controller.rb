34b8b0d4932644199330b1b8548bac6ade92304a
koganeyumi <tamamisan0306@gmail.com>
2021-01-05 22:36:03 +0900

    textモデルにページネーションを追加

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,8 @@
 class TextsController < ApplicationController
+  PER_PAGE = 10
   def index
     genre_list = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
     @texts = Text.where(genre: genre_list)
+    @texts = Text.page(params[:page]).per(PER_PAGE)
   end
 end
