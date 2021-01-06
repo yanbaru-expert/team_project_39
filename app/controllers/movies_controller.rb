12dcea605b2d5342ebb6f5e262a59e92f3673a9d
koganeyumi <tamamisan0306@gmail.com>
2021-01-06 16:17:25 +0900

    view/movies/index.html.erbを修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  PER_PAGE = 10
+  PER_PAGE = 5
   def index
     genre_list = %W(Basic Git HTML&CSS Ruby Ruby on Rails)
     @movies = Movie.where(genre: genre_list).page(params[:page]).per(PER_PAGE)
