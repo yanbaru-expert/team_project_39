d287e984e64f006a9cc7dadac432934beb49a039
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-01-06 22:51:57 +0900

    textコントローラーにransackの機能を追加

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -3,5 +3,8 @@ class TextsController < ApplicationController
   def index
     genre_list = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
     @texts = Text.where(genre: genre_list).page(params[:page]).per(PER_PAGE)
+    
+    @q = Text.ransack(params[:q])
+    @text = @q.result.page(params[:page]).per(PER_PAGE)
   end
 end
