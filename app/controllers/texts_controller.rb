d81b3b73c1de7508c3188b5626f6dcd28dbe100c
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-01-07 23:19:22 +0900

    textコントローラーを修正

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -2,10 +2,9 @@ class TextsController < ApplicationController
   PER_PAGE = 10
   def index
     genre_list = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
-    @texts = Text.where(genre: genre_list).page(params[:page]).per(PER_PAGE)
 
-    @q = Text.ransack(params[:q])
-    @texts = @q.result.page(params[:page]).per(PER_PAGE)
+    @search = Text.ransack(genre: genre_list)
+    @texts = @search.result.page(params[:page]).per(PER_PAGE)
   end
 
 end
