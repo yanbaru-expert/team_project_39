f33e30dad3c7213203f0237c435cb02cd91a5d3b
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-01-09 04:19:26 +0900

    texts_controller.rbとindex.html.erbを修正

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,7 +1,13 @@
 class TextsController < ApplicationController
   PER_PAGE = 10
+
   def index
     genre_list = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
-    @texts = Text.where(genre: genre_list).page(params[:page]).per(PER_PAGE)
+    @search = Text.where(genre: genre_list).ransack(params[:q])
+    @texts = @search.result.page(params[:page]).per(PER_PAGE)
+  end
+
+  def show
+    @text = Text.find(params[:id])
   end
 end
