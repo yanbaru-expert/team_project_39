7815323dfeb7d1bb12106f5991fbd0169b201574
sayaka <salyaka623@gamil.com>
2021-01-09 21:20:50 +0900

    テキスト進捗管理モデルの作成

--- db/migrate/20210109084211_create_text_progresses.rb
+++ db/migrate/20210109084211_create_text_progresses.rb
@@ -2,8 +2,8 @@ class CreateTextProgresses < ActiveRecord::Migration[6.0]
   def change
     create_table :text_progresses do |t|
       t.boolean :complete_flg, null: false, default: false
-      t.references :user_id, null: false, foreign_key: true
-      t.references :text_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :text, null: false, foreign_key: true
 
       t.timestamps
     end
