c940f5ea328548a1331d4ed1bde688941a3ec0cc
kawakami-masachika <supercell1023koeda@gmail.com>
2021-01-09 09:53:23 +0900

    動画進捗管理モデルとの関連付け

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
+  # モデル関連付け
+  has_many :movie_progresses, dependent: :destroy
+  has_many :users, through: :movie_progresses
   validates :genre, presence: true
   validates :title, presence: true
   validates :url, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # モデル関連付け
+  has_many :movie_progresses, dependent: :destroy
+  has_many :movies, through: :movie_progresses
 end
